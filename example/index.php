c80a90b05b944cf8ea3a8e9dec511820c745e8c6
Giuseppe Dess� <thesee@php.net>
2006-03-17 14:27:59 +0000

    initial import
    git-svn-id: https://svn.php.net/repository/pear/packages/HTML_QuickForm_Livesearch/trunk@209540 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ example/index.php
@@ -0,0 +1,58 @@
+<?php
+// $Id$
+require_once 'HTML/QuickForm.php';
+require_once 'HTML/QuickForm/livesearch_select.php';
+require_once 'myfunction.php';
+// Instantiate the HTML_QuickForm object
+$form = new HTML_QuickForm('firstForm');
+
+
+// Add some elements to the form
+$form->addElement('header', null, 'QuickForm example');
+
+$form->addElement('livesearch_select', 'Nome', 'Example: Live Select, <br>select a name and return an ID <br>(useful for dropdown lists)',
+                 array(
+                      'elementId' => 'search', //element id
+                      'callback' => array('Test', 'getTestName'),//callback function to retrieve ID from selection
+//                       'dbh' => $db,//optional handler for callback function
+                      'style' => '',//optional class for style not set or '' ==> default
+                      'divstyle' => '',//optional class for style not set or '' ==> default
+                      'ulstyle' => '',//optional class for style not set or '' ==> default
+                      'listyle' => '',//optional class for style not set or '' ==> default
+                      'searchZeroLength' => 1 //enable the search request with 0 length keyword
+                      )
+                  );
+$form->addElement('livesearch_select', 'Nome2', 'Example2: Live Select, <br>select a name and return an ID <br>(useful for dropdown lists)',
+                 array(
+                      'elementId' => 'search2', //element id
+                      'callback' => array('Test', 'getTestName2'),//callback function to retrieve ID from selection
+//                       'dbh' => $db,//optional handler for callback function
+                      )
+                  );
+$form->addElement('text', 'required_field', 'errorTest', 'Some data:');
+$form->addElement('submit', null, 'View', array('id'=>'submit'));
+$form->addRule('required_field', 'Data required', 'required', null, 'server');
+if ($form->validate()) {
+	echo"<pre>";print_r($_POST);echo"</pre>";die();
+  echo"<pre>";print_r('REDIRECT(stopped obviously) TO: http://www.example.com/'.$form->exportValue('Nome'));echo"</pre>";die();
+  header('Location: http://www.example.com/'.$form->exportValue('Nome'));
+}
+// Output the form
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>Livesearch QuickForm Test</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<p style="font-size:14px;">HTML_AJAX dependent QF_Livesearch DROPDOWN<br>
+In this example we can find something from database, such a Fourm Post or a word in a News Post...<br>
+This type of livesearch is useful when you have a form with a dropdown list with a large number of row.<br>
+With HTML_QuickForm, you can easily do the trick ;).</p>
+<?php
+$form->display();
+?>
+</body>
+</html>
---
+++ example/myfunction.php
@@ -0,0 +1,27 @@
+<?php
+// $Id$
+class Test
+{
+	static $livesearch = array(
+		1 => 'Orange',
+		2 => 'Apple',
+		3 => 'Pear',
+		4 => 'Banana',
+		5 => 'Blueberry'
+		);
+	static $livesearch2 = array(
+		1 => 'Orange-2',
+		2 => 'Apple-2',
+		3 => 'Pear-2',
+		4 => 'Banana-2',
+		5 => 'Blueberry-2'
+		);
+	function getTestName($dbh, $id) {
+		return self::$livesearch[$id];
+	}
+
+	function getTestName2($dbh, $id) {
+		return self::$livesearch2[$id];
+	}
+}
+?>
