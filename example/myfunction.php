14d2b0ecd7ea03ae36ff19f0ab7bfa5c8dde7406
Giuseppe Dess� <thesee@php.net>
2006-06-23 10:08:50 +0000

    update myfunction example to work with php4
    git-svn-id: https://svn.php.net/repository/pear/packages/HTML_QuickForm_Livesearch/trunk@215254 c90b9560-bf6c-de11-be94-00142212c4b1

--- example/myfunction.php
+++ example/myfunction.php
@@ -2,33 +2,29 @@
 // $Id$
 class Test
 {
-    var $livesearch = array();
-    var $livesearch2 = array();
-    function Test() {
-        $this->livesearch2 = array(
-                                1 => 'Orange-2',
-                                2 => 'Apple-2',
-                                3 => 'Pear-2',
-                                4 => 'Banana-2',
-                                5 => 'Blueberry-2'
-                                );
-        $this->livesearch = array(
+    function getTestName($dbh, $id) {
+        $livesearch = array(
                                 1 => 'Orange',
                                 2 => 'Apple',
                                 3 => 'Pear',
                                 4 => 'Banana',
                                 5 => 'Blueberry'
                                 );
-    }
-    function getTestName($dbh, $id) {
         if ($id > 0) {
-            return $this->livesearch[$id];
+            return $livesearch[$id];
         }
     }
 
     function getTestName2($dbh, $id) {
+        $livesearch = array(
+                                1 => 'Orange-2',
+                                2 => 'Apple-2',
+                                3 => 'Pear-2',
+                                4 => 'Banana-2',
+                                5 => 'Blueberry-2'
+                                );
         if ($id > 0) {
-            return $this->livesearch2[$id];
+            return $livesearch2[$id];
         }
     }
 }
